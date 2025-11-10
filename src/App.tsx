63a11a711f61f0430555208583e7a7bcf407c01b
Xiang <lux@gwu.edu>
2025-11-10 12:12:33 -0500

    "Init template from figma"

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { CellPoster } from './components/CellPoster';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <CellPoster />
+    </div>
+  );
+}
